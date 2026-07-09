a260ee613111ef2452156241ee3c87cc1f880dba
LOKESH SONI <lokesh27soni04@gmail.com>
2026-07-09 09:16:11 +0530

    Time: 6 ms (51.63%) | Memory: 60.8 MB (23.57%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    function isAlphaNumeric(ch) {
+        return /[a-zA-Z0-9]/.test(ch);
+    }
+
+    while (left < right) {
+        while (left < right && !isAlphaNumeric(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isAlphaNumeric(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+};
