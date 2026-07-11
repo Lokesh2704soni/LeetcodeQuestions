9f3a4f9521c71906d102e6e8fa3296178869a914
LOKESH SONI <lokesh27soni04@gmail.com>
2026-07-11 23:35:36 +0530

    Time: 53 ms (33.55%) | Memory: 56 MB (91.53%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let slow = head;
+    let fast = head;
+
+    // Detect cycle
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            // Find cycle start
+            let ptr = head;
+
+            while (ptr !== slow) {
+                ptr = ptr.next;
+                slow = slow.next;
+            }
+
+            return ptr;
+        }
+    }
+
+    return null;
+    
+};
