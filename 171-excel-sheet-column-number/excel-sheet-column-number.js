cb37edccba1a6b20b1ab522dab4b6c2332b36461
LOKESH SONI <lokesh27soni04@gmail.com>
2026-08-20 03:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (87.01%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    for(let i= 0; i<columnTitle.length; i++){
+        result *=26;
+        result += columnTitle.charCodeAt(i) - 'A'.charCodeAt(0)+1;
+    }
+    return result;
+};
