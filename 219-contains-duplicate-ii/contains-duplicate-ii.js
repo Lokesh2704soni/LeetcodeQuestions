969203e641c497ef71a5f7e68e336e5f4eb838a3
LOKESH SONI <lokesh27soni04@gmail.com>
2026-08-21 19:57:22 +0530

    Time: 66 ms (9.17%) | Memory: 85.6 MB (14.54%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const seen = new Map();
+    for(i=0;i<nums.length;i++){
+        const num= nums[i];
+        if(seen.has(num) && i-seen.get(num) <=k ){
+            return true;
+        }
+        seen.set(num,i);
+    }
+    return false;
+};
