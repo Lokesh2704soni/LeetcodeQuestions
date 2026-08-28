8c1b6659e2b1f5729120b8d9a090b617fe26c455
LOKESH SONI <lokesh27soni04@gmail.com>
2026-08-28 14:43:47 +0530

    Time: 3 ms (94.77%) | Memory: 60 MB (62.12%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    let vowels = new Set([ 'a' , 'e' , 'i' , 'o' , 'u' , 'A' , 'E' , 'I' , 'O' , 'U']);
+    let arr = s.split('');
+    let left = 0;
+    let right = arr.length - 1;
+
+    while( left < right){
+        if(!vowels.has(arr[left])){
+            left++; 
+        }
+        else if(!vowels.has(arr[right])){
+            right--;
+        }
+        else{
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    return arr.join('');
+};
