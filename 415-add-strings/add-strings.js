be2969f025066b1e930b88c46328fa95fa7022fd
LOKESH SONI <lokesh27soni04@gmail.com>
2026-08-29 13:58:21 +0530

    Time: 2 ms (71.63%) | Memory: 56.5 MB (50.14%) - LeetSync

---
+++ 415-add-strings/add-strings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let carry = 0;
+    let result = '';
+    while(i >= 0 || j >= 0 || carry > 0){
+        let sum = (i >= 0 ? parseInt(num1[i]) : 0) + (j >= 0 ? parseInt(num2[j]) : 0) + carry ;
+        result = (sum % 10) + result;
+        carry = Math.floor(sum / 10);
+        i--;
+        j--;
+    }
+    return result;
+};
